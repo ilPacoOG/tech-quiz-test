2afdcd16e3da8c6acc87ee54f113d511b82a3e89
Michael Fleming <mikahl171@gmail.com>
2024-12-23 23:35:23 -0600

    made numerous changes to /cypress

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,22 +1,19 @@
 import db from '../config/connection.js';
-import { Question } from '../models/index.js';
+import { Question } from '../models/index.js'
 import cleanDB from './cleanDb.js';
 
-// Import JSON data
-import questionData from './pythonQuestions.json';
+import questionData from './pythonQuestions.json' assert{ type: 'json'};
 
-(async () => {
-  try {
-    await db();
-    await cleanDB();
+try {
+  await db();
+  await cleanDB();
 
-    // Bulk insert questions
-    await Question.insertMany(questionData);
+  // bulk create each model
+  await Question.insertMany(questionData);
 
-    console.log('Seeding completed successfully!');
-    process.exit(0);
-  } catch (error) {
-    console.error('Error seeding database:', error);
-    process.exit(1);
-  }
-})();
+  console.log('Seeding completed successfully!');
+  process.exit(0);
+} catch (error) {
+  console.error('Error seeding database:', error);
+  process.exit(1);
+}
